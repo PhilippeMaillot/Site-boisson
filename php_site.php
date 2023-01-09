007ace1d279dddc9cdf86d240561c4eac659847b
Philippe <maillotphilippe78@gmail.com>
2023-01-09 16:04:30 +0100

    php/sql

---
+++ php_site.php
@@ -0,0 +1,10 @@
+<?php
+ $qte= $_GET['Quantite'];
+ $marque= $_GET['Marque'];
+ $prix= $_GET['Prix'];
+ $connexion=new mysqli("localhost","root","","Siteboisson");
+
+ $requete="INSERT INTO produit(marque, quantite, prix) VALUES ('$marque', $qte, $prix)";
+ $result = $connexion->query($requete);
+
+?>
